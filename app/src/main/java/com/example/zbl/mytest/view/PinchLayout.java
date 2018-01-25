86a76d3d2b88f7f7a47468509624c1137f465d1f
zbl <baoliang.zhang@wuliangroup.com>
2018-01-25 10:32:47 +0800

    新增了移动动作的回调接口

--- app/src/main/java/com/example/zbl/mytest/view/PinchLayout.java
+++ app/src/main/java/com/example/zbl/mytest/view/PinchLayout.java
@@ -1,10 +1,12 @@
 package com.example.zbl.mytest.view;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -17,13 +19,36 @@ import android.widget.FrameLayout;
 public class PinchLayout extends FrameLayout {
 
     private static final String TAG = "PinchLayout";
+    private static final float MAX_SCALE = 1.5f;
+    private static final long ANIMATION_DURATION = 200;
 
     private ScaleGestureDetector scaleGestureDetector;
     private ScaleGestureDetector.OnScaleGestureListener onScaleGestureListener;
+    private GestureDetector gestureDetector;
+    private GestureDetector.SimpleOnGestureListener onGestureListener;
     private View childView;
     private float viewX, viewY, viewWidth, viewHeight, translateX, translateY, focusX, focusY;
     private float initX, initY, oX, oY;
     private volatile boolean isPinchBegin = false;
+    private boolean scaleEnable = true;
+
+    private OnChildViewLocationChangedListener listener;
+
+    public interface OnChildViewLocationChangedListener {
+        /**
+         * 子View相对于容器的横向位置
+         *
+         * @param ratio
+         */
+        void childViewMoveScaleX(float ratio);
+
+        /**
+         * 子View是否已经缩小到最小尺寸
+         *
+         * @param isMinSize
+         */
+        void minSize(boolean isMinSize);
+    }
 
     public PinchLayout(@NonNull Context context) {
         super(context);
@@ -53,7 +78,7 @@ public class PinchLayout extends FrameLayout {
                     float scale = detector.getScaleFactor();
                     int width = (int) (viewWidth * scale);
                     int height = (int) (viewHeight * scale);
-                    int maxHeight = (int) (getHeight() * 1.5f);
+                    int maxHeight = (int) (getHeight() * MAX_SCALE);
                     if (height > maxHeight) {
                         width = maxHeight * width / height;
                         height = maxHeight;
@@ -103,6 +128,29 @@ public class PinchLayout extends FrameLayout {
             }
         };
         scaleGestureDetector = new ScaleGestureDetector(context, onScaleGestureListener);
+
+        onGestureListener = new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                childView = getChildAt(0);
+                if (childView != null) {
+                    int width = childView.getWidth();
+                    int height = childView.getHeight();
+                    if (width <= getWidth()) {
+                        int maxHeight = (int) (getHeight() * MAX_SCALE);
+                        width = maxHeight * width / height;
+                        height = maxHeight;
+                        setChildViewLocationSmooth((getWidth() - width) / 2, (getHeight() - height) / 2, width, height);
+                    } else {
+                        height = getWidth() * height / width;
+                        width = getWidth();
+                        setChildViewLocationSmooth((getWidth() - width) / 2, (getHeight() - height) / 2, width, height);
+                    }
+                }
+                return true;
+            }
+        };
+        gestureDetector = new GestureDetector(context, onGestureListener);
     }
 
     @Override
@@ -112,6 +160,12 @@ public class PinchLayout extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (!scaleEnable) {
+            return false;
+        }
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
         childView = getChildAt(0);
         if (childView != null && event.getPointerCount() == 1) {
             switch (event.getAction()) {
@@ -144,6 +198,10 @@ public class PinchLayout extends FrameLayout {
         return scaleGestureDetector.onTouchEvent(event);
     }
 
+    public void setChildViewLocation(float x, float y, int width, int height) {
+        setChildViewLocation(x, y, width, height, "外部调用");
+    }
+
     private void setChildViewLocation(float x, float y, int width, int height, String tag) {
         if (x > 0) {
             x = 0;
@@ -169,7 +227,49 @@ public class PinchLayout extends FrameLayout {
         }
         childView.setX(x);
         childView.setY(y);
+        if (listener != null) {
+            float newDeltaWidth = getWidth() - width;
+            if (newDeltaWidth >= 0) {
+                listener.minSize(true);
+            } else {
+                listener.minSize(false);
+                listener.childViewMoveScaleX(x / newDeltaWidth);
+            }
+
+        }
         showLog(tag + "  x:" + x + ",y:" + y + ",width:" + width + ",height:" + height);
+    }
+
+    private void setChildViewLocationSmooth(final float x, final float y, final int width, final int height) {
+        final float ox = childView.getX();
+        final float oy = childView.getY();
+        final float oWidth = childView.getWidth();
+        final float oHeight = childView.getHeight();
+        ValueAnimator animator = ValueAnimator.ofFloat(0f, 1f);
+        animator.setDuration(ANIMATION_DURATION);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float progress = (float) animation.getAnimatedValue();
+                int dWidth = (int) (oWidth + ((width - oWidth) * progress));
+                int dheight = (int) (oHeight + ((height - oHeight) * progress));
+
+                FrameLayout.LayoutParams layoutParams = (LayoutParams) childView.getLayoutParams();
+                layoutParams.width = dWidth;
+                layoutParams.height = dheight;
+                childView.setLayoutParams(layoutParams);
+                setChildViewLocation(ox + (x - ox) * progress, oy + (y - oy) * progress, dWidth, dheight, "双击动画");
+            }
+        });
+        animator.start();
+    }
+
+    public void setScaleEnable(boolean scaleEnable) {
+        this.scaleEnable = scaleEnable;
+    }
+
+    public void setOnChildViewLocationChangedListener(OnChildViewLocationChangedListener listener) {
+        this.listener = listener;
     }
 
     private void showLog(String msg) {
